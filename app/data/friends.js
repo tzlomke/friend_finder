b0224dff811e0fb8318dde4f6897fbcbcc9f1329
Chieftjz <taylor.zlomke@gmail.com>
2019-01-27 01:18:56 -0700

    renamed friendData in friends.js to friendDataArray to clarify

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,3 @@
-var friendData = [
-    {
-        name: "Taylor",
-        photo: "https://i.kinja-img.com/gawker-media/image/upload/c_fill,fl_progressive,g_center,h_80,q_80,w_80/1860nsgizntrqjpg.jpg",
-        scores: [
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1"
-        ]
-    },
-    {
-        name: "Jimbo",
-        photo: "https://i.kinja-img.com/gawker-media/image/upload/c_fill,fl_progressive,g_center,h_80,q_80,w_80/1860nsgizntrqjpg.jpg",
-        scores: [
-            "2",
-            "1",
-            "1",
-            "1",
-            "5",
-            "1",
-            "3",
-            "1",
-            "1",
-            "1"
-        ]
-    }
-];
+var friendDataArray = [];
 
-module.exports = friendData;
+module.exports = friendDataArray;
