16762fc55a5d08b54979593d82d81416d0d0dcb4
Chieftjz <taylor.zlomke@gmail.com>
2019-01-27 13:32:41 -0700

    added dummy data for first friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,20 @@
-var friendDataArray = [];
+var friendDataArray = [
+    {
+        name: "Taylor",
+        photo: "https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-1/p320x320/42391197_10212552892320035_8048252702448156672_n.jpg?_nc_cat=102&_nc_ht=scontent.fapa1-2.fna&oh=d38465905acff08787255702646f9c14&oe=5CFB381B",
+        scores: [
+            "2",
+            "2",
+            "1",
+            "5",
+            "2",
+            "5",
+            "5",
+            "3",
+            "5",
+            "1"
+        ]
+    }
+];
 
 module.exports = friendDataArray;
