2071c6fce19597d0a321f83c9c77bc1c5327b792
Chieftjz <taylor.zlomke@gmail.com>
2019-01-27 01:10:58 -0700

    completed api POST functionality and route

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,36 @@
-var friendData = [];
+var friendData = [
+    {
+        name: "Taylor",
+        photo: "https://i.kinja-img.com/gawker-media/image/upload/c_fill,fl_progressive,g_center,h_80,q_80,w_80/1860nsgizntrqjpg.jpg",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Jimbo",
+        photo: "https://i.kinja-img.com/gawker-media/image/upload/c_fill,fl_progressive,g_center,h_80,q_80,w_80/1860nsgizntrqjpg.jpg",
+        scores: [
+            "2",
+            "1",
+            "1",
+            "1",
+            "5",
+            "1",
+            "3",
+            "1",
+            "1",
+            "1"
+        ]
+    }
+];
 
 module.exports = friendData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,21 +7,50 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
+
+        // Declares variable for questionaire answers. Used later for comparison
         var userScores = req.body.scores;
+        
+        var comparisonArray = []
+        var yourNewFriend;
 
+        // Loops Through friends.js API data
         for (var i = 0; i < friendData.length; i++) {
-            var totalDifference = 0;
-            var currentFriendScores = friendData[i].scores;
-            console.log("Current Friend Scores: " + currentFriendScores);
-
-            for (var i = 0; i < currentFriendScores.length; i++) {
-                totalDifference += Math.abs(parseInt(currentFriendScores[i]) - parseInt(userScores[i]));
-                console.log("Total Diff: " + totalDifference);
-            };
             
-        }
+            // Declares variable for current array index
+            var currentFriend = friendData[i];
 
+            var totalDifference = 0;
+
+            // Loops through currentFriend.scores and compares to userScores. Adds absolute value of difference to totalDifference
+            for (var x = 0; x < currentFriend.scores.length; x++) {
+                totalDifference += Math.abs(parseFloat(userScores[x]) - parseFloat(currentFriend.scores[x]));
+            }
+
+            // Once currentFriend.scores has been looped over, pushes whole object along with newly defined totalDifference value to comparisonArray
+            comparisonArray.push({
+                name: friendData[i].name,
+                photo: friendData[i].photo,
+                diff: totalDifference
+            });
+            
+        };
+
+        // Sorts Arrays by numeric value of .diff key
+        function sortNumber(a, b) {
+            return a.diff - b.diff;
+        };
+
+        // Applies Sort Function to comparisonArray
+        comparisonArray.sort(sortNumber);
+
+        // Sets yourNewFriend to comparisonArray with lowest diff value
+        yourNewFriend = comparisonArray[0];
+
+        // Pushes Survey Answers to friendData API
         friendData.push(req.body);
-        res.json(true);
+
+        // Returns yourNewFriend object
+        res.json(yourNewFriend);
     });
 };  
